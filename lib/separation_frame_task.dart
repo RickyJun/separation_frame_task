402b2712dea7797230813d6f3901ae23792da282
wenwenjun <wenwenjun@weeget.cn>
2021-05-20 16:34:02 +0800

    first cm

--- lib/separation_frame_task.dart
+++ lib/separation_frame_task.dart
@@ -1,7 +1,3 @@
 library separation_frame_task;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export './frame_task_queue/frame_task_widget.dart';
--- test/separation_frame_task_test.dart
+++ test/separation_frame_task_test.dart
@@ -1,13 +1,17 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:separation_frame_task/separation_frame_task.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    Widget image = Image.network(
+      "something url",
+      width: 100,
+      height: 100,
+    );
+    //just wrap the widget that need to render on next frame
+    image =
+        FrameTaskWidget(image, placeHolderWidth: 100, placeHolderHeight: 100);
   });
 }
